b7fcd635f1fd11b205618c64bbe08cf1eab34f84
KeyForce <july.master@outlook.com>
2020-10-02 15:33:16 +0800

    2020-10-02 15:33:16

--- CUDASample/main.cu
+++ CUDASample/main.cu
@@ -1,6 +1,47 @@
+/*
+ * CLion使用VS作为编译器 平台设置为AMD64
+ * CMakeLists 添加find_package(CUDA) find_package(CUDA REQUIRED)
+*/
 #include <iostream>
-//使用VS作为编译器 平台设置为AMD64
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <math.h>
+// 两个向量加法kernel，grid和block均为一维 只采用一个GPU
+__global__
+void add(int n, float *x, float *y)
+{
+    for (int i = 0; i < n; i++)
+        y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+    int N = 1<<20;
+    float *x, *y;
+
+    // 申请device内存
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+
+    // 申请host内存
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // 执行kernel
+    add<<<1, 1>>>(N, x, y);
+
+    // 保证device和host数据同步，这样不用手动将device得到的结果拷贝到host
+    cudaDeviceSynchronize();
+
+    // 计算误差
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // 释放device内存
+    cudaFree(x);
+    cudaFree(y);
+
     return 0;
 }
